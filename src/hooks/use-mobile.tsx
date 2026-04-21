6a2fd416c0fafca0d9312d4082a34b07c6947782
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-04-21 02:15:17 -0300

    hooks

---
+++ src/hooks/use-mobile.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile() {
+    const [isMobile, setIsMobile] = React.useState(() => {
+        // valor inicial já correto (sem effect)
+        return window.innerWidth < MOBILE_BREAKPOINT;
+    });
+
+    React.useEffect(() => {
+        const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+
+        const onChange = (e: MediaQueryListEvent) => {
+            setIsMobile(e.matches);
+        };
+
+        mql.addEventListener("change", onChange);
+
+        return () => {
+            mql.removeEventListener("change", onChange);
+        };
+    }, []);
+
+    return isMobile;
+}
